c2e37cea0a199af12199133d7d474b57b9f7cd3f
amandamaeheintz <128412214+amandamaeheintz@users.noreply.github.com>
2023-05-09 16:17:28 -0500

    added comments, modified file path names

--- myscript.py
+++ myscript.py
@@ -7,9 +7,9 @@ import time
 # Download NLTK corpora for sentiment analysis
 nltk.download('vader_lexicon')
 
-# Load data from Excel file into a pandas DataFrame
-# Replace 'path_to_file' with the actual path to your Excel file
-df = pd.read_excel('/Users/amandaheintz/Documents/Instruqtdocs/Rawfeedbackdata.xlsx')
+# Load data from Excel file into a pandas DataFrame --- using excel for ease, but data can be loaded from any source, i.e. db, etc. 
+# updatepathtofilehere with actual path to your Excel file
+df = pd.read_excel('updatepathtofilehere.xlsx')
 
 # Create SentimentIntensityAnalyzer object
 sia = SentimentIntensityAnalyzer()
@@ -36,16 +36,16 @@ start_time = time.time()
 df['Sentiment_Category'] = df['Sentiment'].apply(categorize_sentiment)
 print("Sentiment categorization completed. Time elapsed: {:.2f} seconds".format(time.time() - start_time))
 
-# Load pre-trained BERT model and tokenizer
+# Load pre-trained BERT model and tokenizer ------ TODO train model
 start_time = time.time()
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=5)
 print("BERT model loaded. Time elapsed: {:.2f} seconds".format(time.time() - start_time))
 
-# Define the theme labels
+# Define the theme labels - TODO need to update to add OTHER
 theme_labels = ['COURSE CONTENT', 'LEVEL OF DIFFICULTY', 'PACE', 'TECHNICAL ISSUES', 'INSTRUCTOR/DELIVERY']
 
-# Function to classify the feedback into themes using BERT model
+# Classify the feedback into themes using BERT model
 def classify_theme(feedback):
     if isinstance(feedback, str) and feedback.strip() == '':
         return 'N/A'
@@ -71,8 +71,8 @@ df['Feedback_Theme'] = df['Feedback'].apply(classify_theme)
 print("Theme classification completed. Time elapsed: {:.2f} seconds".format(time.time() - start_time))
 
 # Save updated DataFrame to a new Excel file using openpyxl
-# Replace 'output_file.xlsx' with the desired filename and path
+# Replace 'updatefilenamehere.xlsx' with whatever name you want for your ouptut file 
 start_time = time.time()
-df.to_excel('trainedoutput_file.xlsx', index=False, engine='openpyxl')
+df.to_excel('updatefilenamehere.xlsx', index=False, engine='openpyxl')
 print("Output file created. Time elapsed: {:.2f} seconds".format(time.time() - start_time))
 
